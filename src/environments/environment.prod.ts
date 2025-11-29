a7758a400c036bae2e8ecb8bfccde04af9bcbc40
aaronf-tgs <aaronf@thegraphicsstation.com>
2025-11-28 20:53:54 -0600

    Trigger build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // apiUrl: "http://backend-2025-demo.eba-mn2sspi3.us-west-2.elasticbeanstalk.com/"
-  apiUrl: "https://api-west.abcwuatest.org/"
+  apiUrl: "https://api-west.abcwua.org/"
 };
